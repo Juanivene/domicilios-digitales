18fed01174befaaaec65979de0d19699400eb10c
Juanivene <168385555+Juanivene@users.noreply.github.com>
2024-11-29 12:54:36 -0300

    feat: header

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,7 +1,22 @@
+import Grid from "../Grid/Grid";
+
 const Header = () => {
-  return <>
-  
-  </>;
+  return (
+    <>
+      <Grid container gap={1}>
+        <Grid item xs={6} xl={6}>
+          <img
+            className="w-96"
+            src="https://domiciliosdigitales.justucuman.gov.ar/img/logos/logo-corte.png"
+            alt=""
+          />
+        </Grid>
+        <Grid item xs={6} xl={6}>
+          <h1 className="text-right text-2xl font-bold">Domicilio digital</h1>
+        </Grid>
+      </Grid>
+    </>
+  );
 };
 
 export default Header;
--- src/ViewAddresses.tsx
+++ src/ViewAddresses.tsx
@@ -1,10 +1,12 @@
+import Body from "./Components/Body/Body";
 import Header from "./Components/Header/Header";
 
 const ViewAddresses = () => {
   return (
-    <>
+    <div className="mx-20 my-5">
       <Header />
-    </>
+      <Body />
+    </div>
   );
 };
 
