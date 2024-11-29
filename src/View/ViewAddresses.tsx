05e2d02365e212b692f0c18be52ba6a26a261bed
Juanivene <168385555+Juanivene@users.noreply.github.com>
2024-11-29 10:40:05 -0300

    feat: grid implementation

--- src/View/ViewAddresses.tsx
+++
@@ -1,9 +0,0 @@
-const ViewAddresses = () => {
-  return (
-    <div>
-      <button className="btn">Button</button>
-    </div>
-  );
-};
-
-export default ViewAddresses;
---
+++ src/ViewAddresses.tsx
@@ -0,0 +1,11 @@
+import Header from "./Components/Header/Header";
+
+const ViewAddresses = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default ViewAddresses;
