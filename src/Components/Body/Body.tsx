d64dba41a187c68f93e7ebc47726b4185148b363
Juanivene <168385555+Juanivene@users.noreply.github.com>
2024-11-30 17:13:10 -0300

    feat: table and Title and dependencie mui

---
+++ src/Components/Body/Body.tsx
@@ -0,0 +1,11 @@
+import Table from "./Table";
+
+const Body = () => {
+  return (
+    <div>
+      <Table />
+    </div>
+  );
+};
+
+export default Body;
---
+++ src/Components/Body/Table.tsx
@@ -0,0 +1,13 @@
+import ConentTable from "./ContentTable";
+import Title from "./Title";
+
+const Table = () => {
+  return (
+    <>
+      <Title />
+      <ConentTable />
+    </>
+  );
+};
+
+export default Table;
