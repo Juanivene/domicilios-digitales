c214e3c9a660d8f7538b2ffae997c3a47b9b16e9
Juanivene <168385555+Juanivene@users.noreply.github.com>
2024-12-04 09:36:35 -0300

    feat: api function and adapter

--- src/api/addresses.ts
+++ src/api/addresses.ts
@@ -1,9 +1,44 @@
+import { adaptResponse } from "./adapter/adapter";
+export interface Filters {
+  lastName: string | null;
+  name: string | null;
+  profile: string | null;
+}
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
-export const getcustomersFn = async () => {
-  const res = await fetch(`${BACKEND_URL}/customers`);
-  const data = await res.json();
-  if (!res.ok) {
-    throw new Error("Ocurrio un error al obtener los datos");
+
+export const getAddressesFn = async (
+  queryKey: [string, number, Filters?, number?]
+) => {
+  const [, size, filters] = queryKey;
+  let [, , , index] = queryKey;
+  if (!index) index = 0;
+
+  const params = new URLSearchParams();
+  params.append("paging", `${index},${size}`);
+  params.append("sort", "lastname:asc");
+
+  if (filters) {
+    const { name, lastName, profile } = filters;
+    if (name) {
+      params.append("search", `name:${name}`);
+    }
+    if (lastName) {
+      params.append("search", `lastname:${lastName}`);
+    }
+    if (profile) {
+      params.append("search", `profile.name:${profile}`);
+    }
   }
-  return data;
+  const url = `${BACKEND_URL}?${params.toString()}`;
+  console.log(url);
+
+  const res = await fetch(url);
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw new Error("Ocurrió un error al obtener los datos");
+  }
+  const dataAdapted = adaptResponse(data.data);
+
+  return dataAdapted;
 };
