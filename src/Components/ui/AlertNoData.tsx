92bffa702de4c6e759622b47a285866533996db8
Juanivene <168385555+Juanivene@users.noreply.github.com>
2024-12-05 09:14:22 -0300

    feat: feedback info, loader,is error,no data

---
+++ src/Components/ui/AlertNoData.tsx
@@ -0,0 +1,23 @@
+const AlertNoData = () => {
+  return (
+    <div role="alert" className="alert bg-yellowAlert p-2 mt-2">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6 shrink-0 stroke-current"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+        />
+      </svg>
+      No se encontraron resultados con los datos ingresados, intentelo de nuevo
+      o cambie los filtros
+    </div>
+  );
+};
+
+export default AlertNoData;
